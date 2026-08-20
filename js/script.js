ac5904dd410041f531bc27a69fc0c2d80db049bd
sadamsailaja-cyber <sadamsailaja@gmail.com>
2026-08-20 08:59:59 +0000

    architeture done

--- js/script.js
+++
@@ -1,1 +0,0 @@
-alert("Welcome to NRIIT Learning Management System");
---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+let registerForm = document.getElementBYid
+if("registerForm"){
+    registerForm.addEventListener("submit"),function(event){
+        event.preventDefault();
+        console.log(event);
+        let name=document.getElementById("name").Value;
+    }
+}
